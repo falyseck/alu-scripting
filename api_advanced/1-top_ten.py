b15771cef0ad8b8d7b45e9cca752b5eeeff7def4
falyseck <169052940+falyseck@users.noreply.github.com>
2024-10-25 16:07:33 +0000

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
+"""
+1-top_ten
+This module contains a function to query the Reddit API
+and print the titles of the first 10 hot posts for a given subreddit.
+"""
 
 import requests
 
+
 def top_ten(subreddit):
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit. If the subreddit is invalid, prints None.
+    
+    Args:
+        subreddit (str): The name of the subreddit to query.
+    """
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers)

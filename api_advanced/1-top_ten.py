264a4683e1dee5044351334de39c480eeb97131b
falyseck <169052940+falyseck@users.noreply.github.com>
2024-10-25 16:12:46 +0000

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,29 +1,17 @@
 #!/usr/bin/python3
-"""
-1-top_ten
-This module contains a function to query the Reddit API
-and print the titles of the first 10 hot posts for a given subreddit.
-"""
+"""Prints the title of the first 10 hot posts listed for a given subreddit"""
 
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first 10 hot posts
-    listed for a given subreddit. If the subreddit is invalid, prints None.
-    
-    Args:
-        subreddit (str): The name of the subreddit to query.
-    """
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
+    """Main function"""
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
 
-    if response.status_code != 200:
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+    try:
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
+        [print(post.get('data').get('title')) for post in HOT_POSTS]
+    except Exception:
         print(None)
-        return
-
-    posts = response.json().get('data', {}).get('children', [])
-    for post in posts[:10]:
-        print(post['data']['title'])
